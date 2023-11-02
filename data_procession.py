d6ce0c9400013a4c54c2059b4413ba003b6910fd
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-11-02 09:49:56 +0200

    Basic function+unit tests

--- data_procession.py
+++ data_procession.py
@@ -1,8 +1,9 @@
 import requests
-import json
 import time
+import json
 from datetime import datetime
-from dateutil.parser import parse
+
+user_data = []
 
 def get_data(offset):
     url = f"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}"
@@ -11,146 +12,62 @@ def get_data(offset):
     return data['data']
 
 def update_user_data(user, previous_state):
-    if user['isOnline']:
-        if user['userId'] not in previous_state or not previous_state[user['userId']]['isOnline']:
-            user['onlinePeriods'] = previous_state.get(user['userId'], {}).get('onlinePeriods', [])
-            user['onlinePeriods'].append([datetime.now().isoformat(), None])
-        else:
-            user['onlinePeriods'] = previous_state[user['userId']]['onlinePeriods']
-    else:
-        if user['userId'] in previous_state and previous_state[user['userId']]['isOnline']:
-            last_online_period = previous_state[user['userId']]['onlinePeriods'][-1]
-            last_online_period[1] = datetime.now().isoformat()
-            user['onlinePeriods'] = previous_state[user['userId']]['onlinePeriods']
-        else:
-            user['onlinePeriods'] = previous_state.get(user['userId'], {}).get('onlinePeriods', [])
-    return user
+    user_id = user['userId']
+    previous_user = previous_state.get(user_id, {})
+    previous_user.update(user)
+    previous_state[user_id] = previous_user
+    return previous_user
 
-previous_state = {} 
-deleted_users = set()
+previous_state = {}
+
+def save_user_data_to_json(data):
+    with open('user_data.json', 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+def print_json_file(filename):
+    with open(filename, 'r') as json_file:
+        data = json.load(json_file)
+        print(json.dumps(data, indent=4))
 
 def fetch_and_update_data():
     offset = 0
-    all_data = []
     counter = 0
 
     while True:
         data = get_data(offset)
 
-        if not data or counter > 1000: 
+        if not data or counter > 1000:
             break
-        
+
         for d in data:
-            if d['userId'] in deleted_users:
-                continue
-            user = { 'userId': d['userId'], 'isOnline': d['isOnline'], 'lastSeenDate': d['lastSeenDate'] }
+            user_id = d['userId']
+            if user_id not in previous_state:
+                first_detection_time = datetime.now().isoformat()
+            else:
+                first_detection_time = previous_state[user_id].get('firstDetectionTime', None)
+
+            user = {
+                'userId': user_id,
+                'isOnline': d['isOnline'],
+                'lastSeenDate': d['lastSeenDate'],
+                'nickname': d['nickname'],
+                'lastName': d['lastName'],
+                'registrationDate': d['registrationDate'],
+                'firstDetectionTime': first_detection_time,
+            }
             updated_user = update_user_data(user, previous_state)
-            user['totalSecondsOnline'] = calculate_online_time(user)
 
-            if updated_user['userId'] not in [user['userId'] for user in all_data]:
-                all_data.append(updated_user)
-
-            previous_state[updated_user['userId']] = updated_user
+            if updated_user['userId'] not in [user['userId'] for user in user_data]:
+                user_data.append(updated_user)
+                save_user_data_to_json(user_data)
+            
 
         offset += len(data)
         counter += 1
 
-
-    with open('all_data.json', 'w') as f:
-        json.dump(all_data, f)
-
-def calculate_online_time(user):
-    total_seconds_online = 0
-    for period in user['onlinePeriods']:
-        start_time = parse(period[0])
-        end_time = parse(period[1]) if period[1] else datetime.now()
-        total_seconds_online += (end_time - start_time).total_seconds()
-    return total_seconds_online
-
-def calculate_days(user):
-    periods = user['onlinePeriods']
-    if not periods:
-        return 0
-    start_date = parse(periods[0][0]).date()
-    end_date = parse(periods[-1][-1]).date() if periods[-1][-1] else start_date
-    return (end_date - start_date).days + 1
-
-def calculate_average_times(user):
-    days = calculate_days(user)
-    if days == 0:
-        return 0, 0
-    total_seconds_online = calculate_online_time(user)
-    daily_average = total_seconds_online / days
-    weekly_average = daily_average * 7
-    return weekly_average, daily_average
-
-def delete_user_data(user_id):
-    with open('all_data.json', 'r') as f:
-        all_data = json.load(f)
-    all_data = [user for user in all_data if user['userId'] != user_id]
-    with open('all_data.json', 'w') as f:
-        json.dump(all_data, f)
-    deleted_users.add(user_id)
-    if user_id in previous_state:
-        del previous_state[user_id]
-
-def calculate_min_max(user):
-    daily_times = []
-    for period in user['onlinePeriods']:
-        start_time = parse(period[0])
-        end_time = parse(period[1]) if period[1] else datetime.now()
-        daily_times.append((end_time - start_time).total_seconds())
-    return min(daily_times), max(daily_times)
-
-def generate_report(report_name, metrics, users):
-    with open('all_data.json', 'r') as f:
-        all_data = json.load(f)
-    
-    report_data = {}
-    for user in all_data:
-        if user['userId'] not in users:
-            continue
-        user_report = {}
-        if 'dailyAverage' in metrics:
-            _, daily_average = calculate_average_times(user)
-            user_report['dailyAverage'] = daily_average
-        if 'weeklyAverage' in metrics:
-            weekly_average, _ = calculate_average_times(user)
-            user_report['weeklyAverage'] = weekly_average
-        if 'total' in metrics:
-            user_report['total'] = calculate_online_time(user)
-        if 'min' in metrics or 'max' in metrics:
-            min_time, max_time = calculate_min_max(user)
-            if 'min' in metrics:
-                user_report['min'] = min_time
-            if 'max' in metrics:
-                user_report['max'] = max_time
-        report_data[user['userId']] = user_report
-
-    
-    with open(f'{report_name}.json', 'w') as f:
-        json.dump(report_data, f)
-
-    print("Report successfully created.")
-
-def get_reports_in_date_range(report_data, from_date, to_date):
-    reports = []
-
-    for user_id, user_metrics in report_data.items():
-        user_report = {"userId": user_id, "metrics": []}
-        for metric_name, metric_value in user_metrics.items():
-            if metric_name != "total" and isinstance(metric_value, dict):
-                for date, value in metric_value.items():
-                    metric_date = parse(date)
-                    if from_date <= metric_date <= to_date:
-                        user_report["metrics"].append({metric_name: value})
-        
-        if user_report["metrics"]:
-            reports.append(user_report)
-    
-    return reports
-
 if __name__ == "__main__":
     while True:
         fetch_and_update_data()
+        print("User Data JSON:")
+        print_json_file('user_data.json')
         time.sleep(10)
---
+++ tests.py
@@ -0,0 +1,75 @@
+import unittest
+from unittest.mock import Mock, patch
+from data_procession import get_data 
+from data_procession import update_user_data
+from data_procession import fetch_and_update_data
+
+class TestGetDataFunction(unittest.TestCase):
+    @patch('data_procession.requests.get')
+    def test_Should_GetData_When_Success(self, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = {'data': 'mocked_data'}
+        mock_get.return_value = mock_response
+
+        result = get_data(1)
+
+        self.assertEqual(result, 'mocked_data')
+
+    @patch('data_procession.requests.get')
+    def test_Should_ReturnException_When_Failure(self, mock_get):
+        mock_get.side_effect = Exception("Mocked error")
+
+        with self.assertRaises(Exception):
+            get_data(1) 
+
+class TestUpdateUserData(unittest.TestCase):
+
+    def test_Should_Update_UserData_of_ExistingUser(self):
+        previous_state = {'user123': {'userId': 'user123', 'isOnline': True}}
+        user = {'userId': 'user123', 'nickname': 'John', 'isOnline': False}
+
+        updated_user = update_user_data(user, previous_state)
+
+        expected_user = {
+            'userId': 'user123',
+            'isOnline': False,
+            'nickname': 'John'
+        }
+
+        self.assertEqual(updated_user, expected_user)
+
+    def test_Should_Update_UserSata_When_NewUser(self):
+        previous_state = {'user123': {'userId': 'user123', 'isOnline': True}}
+        user = {'userId': 'user456', 'nickname': 'Alice', 'isOnline': True}
+
+        updated_user = update_user_data(user, previous_state)
+
+        expected_user = {
+            'userId': 'user456',
+            'isOnline': True,
+            'nickname': 'Alice'
+        }
+
+        self.assertEqual(updated_user, expected_user)   
+
+
+class TestFetchAndUpdateData(unittest.TestCase):
+    @patch('data_procession.get_data')  
+    @patch('data_procession.update_user_data')  
+    @patch('data_procession.save_user_data_to_json') 
+    def test_Should_fetch_and_update_data(self, mock_save, mock_update, mock_get):
+        mock_get.side_effect = [
+            [{'userId': 1, 'isOnline': True, 'lastSeenDate': '2023-11-02', 'nickname': 'test', 'lastName': 'user', 'registrationDate': '2023-11-01'}],
+            []
+        ]
+        mock_update.return_value = {'userId': 1, 'isOnline': True, 'lastSeenDate': '2023-11-02', 'nickname': 'test', 'lastName': 'user', 'registrationDate': '2023-11-01', 'firstDetectionTime': None}
+        
+        fetch_and_update_data()
+
+        self.assertEqual(mock_get.call_count, 2)
+        self.assertEqual(mock_update.call_count, 1)
+        self.assertEqual(mock_save.call_count, 1)
+if __name__ == '__main__':
+    unittest.main()
+
+
